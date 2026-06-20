e6fd03a36ebfb2fc66fd412196967c5c0d4c1ef0
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-20 18:33:19 -0400

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.95.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = false
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+
+  # These are picked up from environment variables:
+  # ARM_CLIENT_ID, ARM_CLIENT_SECRET
+  # ARM_SUBSCRIPTION_ID, ARM_TENANT_ID
+}
+
+provider "random" {}
+provider "null" {}
